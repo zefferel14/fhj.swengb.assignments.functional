b2de5db78dc832f86074c3b28fcf28fd480a7c88
zefferel14 <lukzefferer@hotmail.de>
2015-11-10 15:07:03 +0100

    done

--- src/main/scala/fhj/swengb/assignments/functional/rladstaetter/FunctionalAssignment.scala
+++ src/main/scala/fhj/swengb/assignments/functional/rladstaetter/FunctionalAssignment.scala
@@ -1,61 +1,60 @@
-package fhj.swengb.assignments.functional.rladstaetter
+package fhj.swengb.assignments.functional.lzefferer
 
 /**
-  * This is the first assignment for swengb 2015.
-  *
-  */
+ * This is the first assignment for swengb 2015.
+ *
+ */
 case class FunctionalAssignment(lowerBound: Int = 1,
                                 upperBound: Int = 1000,
                                 factor: Int = 2) {
   require(lowerBound < upperBound)
 
   /**
-    * implement a sequence of ints, which start with lowerBound and end with
-    * upperbound.
-    *
-    * for all elements following is true:
-    *
-    * xs(i) < xs(i+1)
-    * xs(i) + factor == xs(i + 1) (for i > 0 and i <= 1000)
-    *
-    */
-  val xs: Seq[Int] = ???
+   * implement a sequence of ints, which start with lowerBound and end with
+   * upperbound.
+   *
+   * for all elements following is true:
+   *
+   * xs(i) < xs(i+1)
+   * xs(i) + 1 == xs(i + 1) (for i > 0 and i <= 1000)
+   *
+   */
+  val xs: Seq[Int] = lowerBound to upperBound
 
   /**
-    * provide the sum of all elements defined in xs
-    */
-  val sumXs = ???
+   * provide the sum of all elements defined in xs
+   */
+  val sumXs = xs.sum
 
   /**
-    * This sequence contains a list with the following elements:
-    *
-    * x(0)   = (upperbound) * factor
-    * x(1)   = (upperbound - 1) * factor
-    * x(2)   = (upperbound - 2) * factor
-    *
-    * ... goes on and on ...
-    *
-    * x(998) = factor
-    * x(999) = lowerbound
-    *
-    */
-  val ys: Seq[Int] = ???
+   * This sequence contains a list with the following elements:
+   *
+   * x(0)   = (upperbound) * factor
+   * x(1)   = (upperbound - 1) * factor
+   * x(2)   = (upperbound - 2) * factor
+   *
+   * ... goes on and on ...
+   *
+   * x(998) = factor
+   * x(999) = lowerbound
+   *
+   */
+  val ys: Seq[Int] = xs.reverse.map(_*factor)
 
   /**
-    * this sequence contains pairs of xs and ys. the ys should
-    * be in reversed order.
-    */
-  val zs: Seq[(Int, Int)] = ???
+   * this sequence contains pairs of xs and ys. the ys should
+   * be in reversed order.
+   */
+  val zs: Seq[(Int, Int)] = xs zip ys.reverse
 
   /**
-    * Function should divide the second operand with the first.
-    */
-  def f(ab: (Int, Int)): Int = ???
+   * Function should divide the second operand with the first.
+   */
+  def f(ab: (Int, Int)): Int = ab._2 / ab._1
 
   /**
-    * apply function f defined above to all elements of zs, return the sequence
-    */
-  val result: Seq[Int] = ???
+   * apply function f defined above to all elements of zs, return the sequence
+   */
+  val result: Seq[Int] = zs.map(f)
 
 }
-
